b8e33a6bbaf691428693030219afc2fd4312848a
Butch Bet <butchbet01@gmail.com>
2022-10-07 12:19:16 -0500

    Created the section UI for the about section, the next step is to create one script to toggled the hidding of the pdf when the user click check my cv.

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,6 +2,7 @@ const aboutSelector = document.getElementById('aboutSelector');
 const porjectsSelector = document.getElementById('porjectsSelector');
 const contactSelector = document.getElementById('contactSelector');
 
+// Script to prevent the default behavour of some elements
 [aboutSelector, porjectsSelector, contactSelector].forEach((element) => {
     element.addEventListener('click', (e) => {
         e.preventDefault();
