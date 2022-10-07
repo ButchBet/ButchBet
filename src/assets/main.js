b5926f1ee073299c8137e9c180eb52715cc838c7
Butch Bet <butchbet01@gmail.com>
2022-10-07 12:31:21 -0500

    Created general script to do different things with different elements with the click event, the first one has been the clicked cv button.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,11 +1,34 @@
 const aboutSelector = document.getElementById('aboutSelector');
 const porjectsSelector = document.getElementById('porjectsSelector');
 const contactSelector = document.getElementById('contactSelector');
+const cvButton = document.getElementById('cvButton');
+const pdfJsViewer = document.getElementById('pdfJsViewer');
 
 // Script to prevent the default behavour of some elements
 [aboutSelector, porjectsSelector, contactSelector].forEach((element) => {
     element.addEventListener('click', (e) => {
         e.preventDefault();
         alert('Hello world');
+    });
+});
+
+// Script to create the insteraction in some elements when get clicked
+[cvButton,].forEach((element) => {
+    element.addEventListener('click', (e) => {
+        switch (e.target.id) {
+            case 'cvButton':
+                    pdfJsViewer.classList.toggle('hidden');
+
+                    // condition to change the text contenct in the cv button
+                    if(!pdfJsViewer.classList.contains('hidden')) {
+                        cvButton.textContent = 'Hidde my cv';
+                    } else {
+                        cvButton.textContent = 'Check my cv';
+                    }
+                break;
+    
+            default:
+                break;
+        }    
     });
 });
