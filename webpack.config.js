2d6d0e25aa6fc15d031c005239cd7b01188239d1
Butch Bet <butchbet01@gmail.com>
2022-11-22 17:42:26 -0500

    Made the installation of the needed dependencies.

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+        publicPath: "/",
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@styles': path.resolve(__dirname, 'src/styles'),
+            "@images": path.resolve(__dirname, "src/assets/images"),
+            "@containers": path.resolve(__dirname, "src/containers"),
+            "@routes": path.resolve(__dirname, "src/routes"),
+            "@pages": path.resolve(__dirname, "src/pages"),
+            "@hooks": path.resolve(__dirname, "src/hooks"),
+            "@context": path.resolve(__dirname, "src/context")
+        },
+
+    },
+
+    mode: 'development',
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+
+            {
+                test: /\.html$/,
+                use: [
+                    { loader: 'html-loader' }
+                ]
+            },
+
+            {
+              test: /\.(s[ac]ss|css)$/,
+              use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+              ]
+            },
+
+            {
+                test: /\.(png|svg|jp?g|gif)$/i,
+                use: [
+                    {
+                      loader: 'url-loader',
+                      options: {
+                        limit: 10000
+                      }
+                    }
+                ]
+            }
+        ],
+    },
+
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html',
+        }),
+
+        new miniCssExtractPlugin({
+          filename: '[name].css',
+        }),
+    ],
+}
