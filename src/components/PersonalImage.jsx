2f2cf986706eec4915c0ed95ca3f00184777cdf7
Butch Bet <butchbet01@gmail.com>
2023-02-01 16:59:45 -0500

    Updated source of the personal image url.

--- src/components/PersonalImage.jsx
+++ src/components/PersonalImage.jsx
@@ -4,7 +4,7 @@ import "@styles/personalImage.css";
 const PersonalImage = () => {
     return (
         <figure className="personalImage mx-auto max-w-7xl px-4 sm:px-6lg:px-8">
-            <img className="personalImage__img" src="https://i.ibb.co/Mpnx5tw/personal-Image.jpg" alt="Image of Alejandro" />
+            <img className="personalImage__img" src="https://i.postimg.cc/KYzBGybD/photo-2023-02-01-16-57-46.jpg" alt="Image of Alejandro" />
         </figure>
     );
 }
