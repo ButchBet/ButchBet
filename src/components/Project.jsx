57bc952fe1c6aff87aeac0461819ca126ccefa7b
Butch Bet <butchbet01@gmail.com>
2023-02-04 01:26:26 -0500

    Implemented a technologies section layout, the  next step is to style it.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,11 +6,35 @@ import "@styles/project.css";
 
 import github from "@images/github.svg";
 import live from "@images/internet.png";
+import html5 from "@images/html5.png";
+import css from "@images/css3.png";
+import javascript from "@images/javascript.png";
+import bootstrap from "@images/bootstrap.png";
+import webcomponents from "@images/webcomponents.png";
+import reactjs from "@images/reactjs.png";
+import webpack from "@images/webpack.png";
 
 const Project = ({project}) => {
     const {goLeft, goRight} = React.useContext(ProjectContext);
 
     const [index, setIndex] = React.useState(0);
+
+    // function setImage(name) {
+    //     switch (name) {
+    //         case "html5":
+    //                 console.log("Html5 earned.")
+    //                 return html5;
+    //             break;
+
+    //         // case "html5":
+    //         //         return html5;
+    //         //     break;
+        
+    //         default:
+    //             break;
+    //     }
+    // }
+
     return (
         <div className="project">
             <h2 className="project__title">{project.title}</h2>
@@ -27,6 +51,30 @@ const Project = ({project}) => {
             </section>
             
             <p className="project__description">{project.description}</p>
+
+            <section className="technologies">
+                {
+                    project.technologies.map((technology) => {
+                        return (<img src={
+                            technology.toLowerCase() === "html"
+                            ? html5
+                            : technology.toLowerCase() === "css"
+                            ? css
+                            : technology.toLowerCase() === "javascript"
+                            ? javascript
+                            : technology.toLowerCase() === "bootstrap"
+                            ? bootstrap
+                            : technology.toLowerCase() === "webcomponents"
+                            ? webcomponents
+                            : technology.toLowerCase() === "reactjs"
+                            ? reactjs
+                            : technology.toLowerCase() === "webpack"
+                            ? webpack
+                            : "Error"
+                        } alt={`${technology} Icon`} className="technologies__item"  key={`item__${technology.toLowerCase()}`} title={technology} />)
+                    })
+                }
+            </section>
             
             <section className="project__choose">
                 <figure className="icons">
